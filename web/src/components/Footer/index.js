f5ddf995ce58a5b979136580c962203cfe1abba3
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-05-04 14:05:24 -0300

    First Commit

---
+++ web/src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './styles.css';
+
+export default function Footer() {
+
+  return (
+    <>
+    <div className="footer">
+      <p>
+        Copyright © OnTimeBus - 2020
+      </p>
+    </div>
+    </>
+  )
+}
